8be36fb457bf6beb4fd432a17d6aecd0f9951fb3
Aiden D <aiden-deloryn@users.noreply.github.com>
2015-12-12 15:07:58 +1000

    Implemented player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,31 +2,36 @@
 using System.Collections;
 
 public class Player : MonoBehaviour, IInputObserver {
-	public float jumpPower = 200f;
 	public int maxJumps = 1;
+	public float jumpPower = 200f;
+	public float moveSpeed = 10f;
 
 	[SerializeField]
 	private InputManager inputManager;
 
 	private Rigidbody2D rigidbody;
 	private int jumpsRemaining;
-
-	// Use this for initialization
+	private Vector3 moveDirection = Vector3.zero;
+	
 	void Start () {
 		inputManager.AddObserver (this);
 		this.rigidbody = gameObject.GetComponent<Rigidbody2D> ();
 		jumpsRemaining = maxJumps;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+		gameObject.transform.position += new Vector3(moveDirection.x * Time.deltaTime, 
+		                                             moveDirection.y * Time.deltaTime, 
+		                                             0f);
 	}
 
 	public void ButtonPressed(InputButton button) {
 		switch (button) {
 		case InputButton.Left:
-
+			moveDirection += new Vector3(-moveSpeed, 0f, 0f);
+			break;
+		case InputButton.Right:
+			moveDirection += new Vector3(moveSpeed, 0f, 0f);
 			break;
 		case InputButton.Jump:
 			if (jumpsRemaining-- > 0) this.rigidbody.AddForce(new Vector2(0f, jumpPower));
@@ -35,7 +40,14 @@ public class Player : MonoBehaviour, IInputObserver {
 	}
 
 	public void ButtonReleased(InputButton button) {
-
+		switch (button) {
+		case InputButton.Left:
+			moveDirection -= new Vector3(-moveSpeed, 0f, 0f);
+			break;
+		case InputButton.Right:
+			moveDirection -= new Vector3(moveSpeed, 0f, 0f);
+			break;
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
