bc0391b530ba1c1f7ff17eb11fbb257732867f9d
Aiden D <aiden-deloryn@users.noreply.github.com>
2015-12-13 14:51:27 +1000

    Implemented enemy eye movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
+	public GameObject eyes;
+
 	private bool attacking = false;
 	private float lastAttackTime = 0f;
 	private float attackInterval = 2f;
@@ -16,10 +18,16 @@ public class Enemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (attacking) {
+			Vector3 playerPos = GameObject.FindGameObjectWithTag ("Player").transform.position;
+			Vector3 eyeDirection = (playerPos - eyes.transform.position).normalized;
+			eyes.transform.position = transform.position + new Vector3(eyeDirection.x * 0.5f, eyeDirection.y * 0.5f, eyeDirection.z);
+		}
+
 		if (((Time.realtimeSinceStartup - lastAttackTime) >= attackInterval) && attacking) {
 			GameObject bulletInstance = Instantiate (bullet) as GameObject;
 			bulletInstance.transform.position = transform.position;
-			bulletInstance.GetComponent<EnemyBullet>().Target = MultiTags.FindGameObjectWithTag(Tag.Player);
+			bulletInstance.GetComponent<EnemyBullet>().Target = GameObject.FindGameObjectWithTag ("Player");
 			Destroy (bulletInstance, 3f);
 			lastAttackTime = Time.realtimeSinceStartup;
 		}
@@ -29,7 +37,7 @@ public class Enemy : MonoBehaviour {
 		Player player = other.gameObject.GetComponent<Player> ();
 
 		if (player != null) {
-			attacking = true;
+			BeginAttacking();
 		}
 	}
 
@@ -37,15 +45,17 @@ public class Enemy : MonoBehaviour {
 		Player player = other.gameObject.GetComponent<Player> ();
 		
 		if (player != null) {
-			attacking = false;
+			StopAttacking();
 		}
 	}
 
 	private void BeginAttacking() {
 		attacking = true;
+
 	}
 
 	private void StopAttacking() {
 		attacking = false;
+		eyes.transform.position = transform.position;
 	}
 }
